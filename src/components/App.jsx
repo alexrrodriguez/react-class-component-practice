2e4e8ef67fc05ca082a262548481d75994a3719c
alex <arodriguez202020@gmail.com>
2021-12-10 11:06:07 -0600

    create counter using functional component with useState, import component and render in aApp.jsx

---
+++ src/components/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import FunctionalComponent from "./FunctionalComponent";
+
+class App extends React.Component {
+  render() {
+    return <FunctionalComponent />;
+  }
+}
+
+export default App;
